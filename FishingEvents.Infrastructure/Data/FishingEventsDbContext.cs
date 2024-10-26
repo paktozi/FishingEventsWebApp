bc41e341bace9ff28e1fc71d824bcf4657b6cb2b
paktozi <130216112+paktozi@users.noreply.github.com>
2024-10-26 19:47:04 +0300

    Scaffold Identity

--- FishingEvents.Infrastructure/Data/FishingEventsDbContext.cs
+++ FishingEvents.Infrastructure/Data/FishingEventsDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FishingEventsApp.Infrastructure
 {
-    public class FishingEventsDbContext : IdentityDbContext
+    public class FishingEventsDbContext : IdentityDbContext<IdentityUser>
     {
         public FishingEventsDbContext(DbContextOptions<FishingEventsDbContext> options)
             : base(options)
