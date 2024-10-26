bdc097fb9ab976f4be7ee6f3cde68275f0210570
paktozi <130216112+paktozi@users.noreply.github.com>
2024-10-26 18:58:44 +0300

    Move DbContext to Infrastructure.Data

---
+++ FishingEvents.Infrastructure/Data/FishingEventsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FishingEvents.Infrastructure.Data
+{
+    public class FishingEventsDbContext : IdentityDbContext
+    {
+        public FishingEventsDbContext(DbContextOptions<FishingEventsDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- FishingEventsWebApp/Program.cs
+++ FishingEventsWebApp/Program.cs
@@ -1,4 +1,4 @@
-using FishingEventsWebApp.Data;
+using FishingEvents.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,12 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<FishingEventsDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<FishingEventsDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
