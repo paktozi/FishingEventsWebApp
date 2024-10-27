1029635843650a916f4de8f1772276f5646a7b02
paktozi <130216112+paktozi@users.noreply.github.com>
2024-10-27 10:28:37 +0200

    Added Participant table

---
+++ FishingEvents.Infrastructure/Data/Models/Participant.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishingEvents.Infrastructure.Data.Models
+{
+    public class Participant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = String.Empty;
+
+        [Required]
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = null!;
+
+        [Required]
+        public IdentityUser? User { get; set; }
+
+    }
+}
